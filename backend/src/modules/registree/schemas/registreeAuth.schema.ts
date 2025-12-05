6ebbf0db2b4067a82fa6db5f981565f57e546473
THaniel000 <189817952+THaniel000@users.noreply.github.com>
2025-12-05 08:15:49 +0800

    Delete backend/src/modules/registree/schemas directory

--- backend/src/modules/registree/schemas/registreeAuth.schema.ts
+++
@@ -1,7 +0,0 @@
-import z from "zod";
-
-export const registreeVerifySchema = z.object({
-  id: z.string().nonempty().uuid("Invalid ID format"),
-});
-
-export type RegistreeVerifyDTO = z.infer<typeof registreeVerifySchema>;
