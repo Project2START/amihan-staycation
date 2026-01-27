3f930fb213a1d9fd747c5c6d9a3765cd50d2e3da
THaniel000 <189817952+THaniel000@users.noreply.github.com>
2026-01-27 12:56:54 +0800

    Admin folders

--- backend/src/modules/product/services/product.service.ts
+++ backend/src/modules/product/services/product.service.ts
@@ -7,48 +7,47 @@ export class ProductService {
   async create(newProduct: ProductDTO, photos: Express.Multer.File[]) {
     const { name, maxPersons, price, about, attributes } = newProduct;
 
-    const product = await productRepository.create({
-      name,
-      maxPersons,
-      price,
-      about,
-      attributes,
-    });
+    console.log(newProduct);  
+    // const product = await productRepository.create({
+    //   name,
+    //   maxPersons,
+    //   price,
+    //   about,
+    //   attributes,
+    // });
 
-    const uploadedFiles: {
-      path: string;
-      url: string;
-      order: number;
-    }[] = [];
+    // const uploadedFiles: {
+    //   path: string;
+    //   url: string;
+    //   order: number;
+    // }[] = [];
 
-    for (let i = 0; i < photos.length; i++) {
-      const file = photos[i];
+    // for (let i = 0; i < photos.length; i++) {
+    //   const file = photos[i];
 
-      const ext = file.originalname.split(".").pop();
-      const fileName = `${crypto.randomUUID()}.${ext}`;
-      const filePath = `products/${fileName}`;
+    //   const ext = file.originalname.split(".").pop();
+    //   const fileName = `${crypto.randomUUID()}.${ext}`;
+    //   const filePath = `products/${fileName}`;
 
-      const { error } = await supabase.storage
-        .from("images")
-        .upload(filePath, file.buffer, {
-          contentType: file.mimetype,
-        });
+    //   const { error } = await supabase.storage
+    //     .from("images")
+    //     .upload(filePath, file.buffer, {
+    //       contentType: file.mimetype,
+    //     });
 
-      if (error) throw error;
+    //   if (error) throw error;
 
-      const { data } = supabase.storage.from("images").getPublicUrl(filePath);
+    //   const { data } = supabase.storage.from("images").getPublicUrl(filePath);
 
-      uploadedFiles.push({
-        path: filePath, // store in DB
-        url: data.publicUrl, // send to frontend
-        order: i + 1,
-      });
-    }
+    //   uploadedFiles.push({
+    //     path: filePath, // store in DB
+    //     url: data.publicUrl, // send to frontend
+    //     order: i + 1,
+    //   });
+    // }
 
-    await productRepository.createMultiplePhotos({
-      alt: "Amihan Staycation Unit Photo",
-    });
-    console.log(uploadedFiles);
+    // // 
+    // console.log(uploadedFiles);
 
     // console.log(newProduct, photos);
   }
