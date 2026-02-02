acb9e0cfc9c94ca2fa227f8a22bb333833d13584
gregor <markgregory2004@gmail.com>
2026-02-02 23:10:17 +0800

    banner and text Size adjustment

--- frontend/src/app/(admin)/spaces/page.tsx
+++ frontend/src/app/(admin)/spaces/page.tsx
@@ -5,6 +5,17 @@ export default function page() {
   return (
     <div className="flex flex-col">
       <HeaderAdmin />
+      {/* Banner Section */}
+      <div className="bg-secondary-normal px-6 py-4 md:py-20 lg:py-24">
+        <div className="flex flex-col items-center justify-center text-center gap-1">
+          <h1 className="text-xl font-bold text-white md:text-4xl lg:text-5xl">
+            Your Staycation Spaces
+          </h1>
+          <p className="text-xs text-white md:text-lg lg:text-xl">
+            Manage your perfect getaways in one place.
+          </p>
+        </div>
+      </div>
       <div></div>
       <NavigationBottomAdmin />
     </div>
