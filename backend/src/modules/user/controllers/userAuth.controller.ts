1ce9e65020af3a26aa2003403cf305dec30e6957
Arjohn15 <arjohn15@gitAccount>
2026-03-23 18:38:46 +0800

    Test to find bug

--- backend/src/modules/user/controllers/userAuth.controller.ts
+++ backend/src/modules/user/controllers/userAuth.controller.ts
@@ -27,6 +27,8 @@ class UserAuthController {
 
     res.cookie("auth_token", jwt_token, cookieOptions(24 * 60 * 60 * 1000));
 
+    console.log(jwt_token, req.secure);
+
     res.status(201).json({ message: "Account successfully created" });
   }
   async signIn(req: Request, res: Response) {
